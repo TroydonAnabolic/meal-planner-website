e1cc32d551e4cd1bed735b79e6b77831a3d48f23
Troydon <50076962+TroydonAnabolic@users.noreply.github.com>
2025-01-25 10:39:50 +1300

    Fix some styling

--- app/Header.tsx
+++ app/Header.tsx
@@ -53,7 +53,7 @@ const Header = () => {
     <header className="absolute inset-x-0 top-0 z-50">
       <nav
         aria-label="Global"
-        className="flex items-center justify-between p-6 lg:px-8"
+        className="flex items-center justify-between p-3 lg:px-8"
       >
         <div className="flex lg:flex-1">
           <CompanyLogo />
@@ -92,7 +92,7 @@ const Header = () => {
         <DialogPanel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
           <div className="flex items-center justify-between">
             <a href="#" className="-m-1.5 p-1.5">
-              <span className="sr-only">Your Company</span>
+              <span className="sr-only">Meal Planner</span>
               <Image
                 src="/meal-planner-logo.png"
                 alt="AI Trainer"
