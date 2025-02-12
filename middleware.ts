00a50290244c6ac78b1bdfdea62fce72607f4318
Troydon <50076962+TroydonAnabolic@users.noreply.github.com>
2025-02-13 11:56:21 +1300

    remove middleware edit

--- middleware.ts
+++ middleware.ts
@@ -83,18 +83,7 @@ export async function middleware(req: NextRequest) {
     }
   }
 
-  var response = NextResponse.next();
-  response.headers.set("Access-Control-Allow-Origin", "*");
-  response.headers.set(
-    "Access-Control-Allow-Methods",
-    "GET, POST, PUT, DELETE, OPTIONS"
-  );
-  response.headers.set(
-    "Access-Control-Allow-Headers",
-    "Content-Type, Authorization"
-  );
-
-  return response;
+  return NextResponse.next();
 }
 
 /**
