b613381ffa9aaeaebdeff33e156740d5c639aa2d
Troydon <50076962+TroydonAnabolic@users.noreply.github.com>
2025-02-18 18:13:49 +1300

    update json ld

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,7 @@ import SessionProvider from "./components/auth/session-provider";
 import { auth } from "@/auth";
 import { Session } from "next-auth";
 import Script from "next/script";
+import Head from "next/head";
 
 export const metadata: Metadata = {
   title: "Meal Planner",
@@ -20,106 +21,36 @@ export default async function RootLayout({
   children: React.ReactNode;
 }>) {
   const session = (await auth()) as Session;
-  const jsonLd = [
-    {
-      "@context": "http://schema.org",
-      "@type": "Product",
-      url: "https://mealplanner.smartaitrainer.com/",
-      brand: {
-        "@type": "Brand",
-        logo: "https://mealplanner.smartaitrainer.com/_next/image?url=%2Fmeal-planner-logo.png&w=256&q=75",
-      },
+  const jsonLd = {
+    "@context": "http://schema.org",
+    "@type": "Product",
+    name: "MEAL PLANNER",
+    description: "Personalized Diet Plans Just for You",
+    url: "https://mealplanner.smartaitrainer.com/",
+    brand: {
+      "@type": "Brand",
+      logo: "https://mealplanner.smartaitrainer.com/_next/image?url=%2Fmeal-planner-logo.png&w=256&q=75",
     },
-    {
-      "@context": "http://schema.org",
-      "@type": "Product",
-      url: "https://mealplanner.smartaitrainer.com/how-it-works",
+    offers: {
+      "@type": "Offer",
+      price:
+        "Discover the perfect diet tailored to your needs. Whether you want to lose weight, gain muscle, or just eat healthier, we've got you covered, starting from $19/month.",
     },
-    {
-      "@context": "http://schema.org",
-      "@type": "Product",
-      url: "https://mealplanner.smartaitrainer.com/demo",
-    },
-    {
-      "@context": "http://schema.org",
-      "@type": "Product",
-      url: "https://mealplanner.smartaitrainer.com/login",
-    },
-    {
-      "@context": "http://schema.org",
-      "@type": "Product",
-      url: "https://mealplanner.smartaitrainer.com/#",
-    },
-    {
-      "@context": "http://schema.org",
-      "@type": "Product",
-      name: "Personalized",
-      description: "tailored",
-      url: "https://mealplanner.smartaitrainer.com/register",
-    },
-    {
-      "@context": "http://schema.org",
-      "@type": "Product",
-      image: "https://mealplanner.smartaitrainer.com/aiimages/food/protein.svg",
-      url: "https://mealplanner.smartaitrainer.com/how-it-works",
-    },
-    {
-      "@context": "http://schema.org",
-      "@type": "Product",
-      url: "https://mealplanner.smartaitrainer.com/how-it-works",
-      brand: {
-        "@type": "Brand",
-        logo: "https://mealplanner.smartaitrainer.com/_next/image?url=%2Fmeal-planner-logo.png&w=256&q=75",
-      },
-    },
-    {
-      "@context": "http://schema.org",
-      "@type": "Product",
-      url: "https://mealplanner.smartaitrainer.com/meal-planner/food/browse-food/",
-    },
-    {
-      "@context": "http://schema.org",
-      "@type": "Product",
-      url: "https://mealplanner.smartaitrainer.com/premium-plan",
-    },
-    {
-      "@context": "http://schema.org",
-      "@type": "Product",
-      url: "https://mealplanner.smartaitrainer.com/contact-us",
-    },
-    {
-      "@context": "http://schema.org",
-      "@type": "Product",
-      url: "https://mealplanner.smartaitrainer.com/about-us",
-    },
-    {
-      "@context": "http://schema.org",
-      "@type": "Product",
-      url: "https://mealplanner.smartaitrainer.com/#",
-    },
-    {
-      "@context": "http://schema.org",
-      "@type": "Product",
-      url: "https://mealplanner.smartaitrainer.com/privacy",
-    },
-    {
-      "@context": "http://schema.org",
-      "@type": "Product",
-      url: "https://mealplanner.smartaitrainer.com/terms",
-    },
-  ];
+  };
   return (
     <html lang="en">
-      <head>
-        <Script
-          src="https://developer.edamam.com/attribution/badge.js"
-          strategy="afterInteractive"
-        />
-        <script
-          type="application/ld+json"
-          dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
-        />
-      </head>
+      <Head>
+        <>
+          <Script
+            src="https://developer.edamam.com/attribution/badge.js"
+            strategy="afterInteractive"
+          />
+          <script
+            type="application/ld+json"
+            dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
+          />
+        </>
+      </Head>
       <body className="flex flex-col min-h-screen">
         <SessionProvider basePath="" session={session}>
           <Header /> {/* Only render NavBar if not in noNavRoutes */}
